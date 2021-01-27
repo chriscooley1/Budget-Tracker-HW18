6e2ca3ec2e1a7ca60e5069302c6ac68c58fd2487
Chris Cooley <68765420+chriscooley1@users.noreply.github.com>
2021-01-26 21:48:13 -0700

    added info for heroku deployment

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ function shouldCompress (req, res) {
     // don't compress responses with this request header
     return false
   }
+ 
   // fallback to standard filter function
   return compression.filter(req, res)
 }
@@ -26,10 +27,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
